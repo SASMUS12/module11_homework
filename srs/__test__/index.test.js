54ac5d16a41b3790645cd34d733e43c48c2ad003
SASMUS12 <85361279+SASMUS12@users.noreply.github.com>
2021-12-02 20:25:23 +0300

    Update index.test.js

--- srs/__test__/index.test.js
+++ srs/__test__/index.test.js
@@ -1,3 +1,12 @@
-export function multiply(a, b) {
-    return a * b;
-}
+import { multiply } from "../index.js";
+
+ xdescribe("test multiply", () => {
+    it("multiply 1 * 0 to equal 1'", () => {
+        const result = multiply(1, 0)
+        expect(result).toBe(0);
+    }),
+        it("multiply 1 * 1 to equal 1'", () => {
+            const result = multiply(1, 1)
+            expect(result).toBe(1);
+        });
+});
